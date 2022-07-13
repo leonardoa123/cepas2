bd8c563ea85e0aa7be265dcaf603377b3d8598fa
Germán Trigo Cortés <contacto@alphacorp.cl>
2022-07-13 02:32:54 -0400

    V7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
-  resources :vinos
-  resources :cepas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root 'home#index', as: 'home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root 'home#index', as: 'user_home'
+
+  devise_for :admins
+  get 'vinos/index', as: 'admin_vinos'
+  get 'cepas/index', as: 'admin_cepas'
+  get 'home/index', as: 'admin_home'
+
+  authenticate :admin do
+    resources :vinos
+    resources :cepas
+  end
 end
